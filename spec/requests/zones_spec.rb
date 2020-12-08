6dfb635ab5aae645eb0a38494752d634e8684f5b
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-12-08 16:38:05 -0500

    Add Additional Tests
    * Add tests for additional form routes.

--- spec/requests/zones_spec.rb
+++ spec/requests/zones_spec.rb
@@ -5,8 +5,30 @@ RSpec.describe "Zones", type: :request do
     it "works!" do
       FactoryBot.create(:zone)
       get zones_path
-      binding.irb
       expect(response).to have_http_status(200)
+    end
+  end
+  
+  describe "POST /zones" do
+    it "works!" do
+      post zones_path, params: { zone: FactoryBot.attributes_for(:zone) }
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
+  describe "PATCH/PUT /zones" do
+    it "works!" do
+      zone = FactoryBot.create(:zone)
+      put "/zones/#{zone.id}", params: { zone: FactoryBot.attributes_for(:zone) }
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
+  describe "DELETE /zones" do
+    it "works!" do
+      zone = FactoryBot.create(:zone)
+      delete "/zones/#{zone.id}"
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
