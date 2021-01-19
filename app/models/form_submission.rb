46f0eb55341dba00e5c64f225fe442a82e3e7050
Matthew Zagaja <mzagaja@alum.wpi.edu>
2021-01-19 14:51:45 -0500

    Resolve #57 Slack Notifications
    * Notify slack when we get a new submission saved.

--- app/models/form_submission.rb
+++ app/models/form_submission.rb
@@ -1,3 +1,17 @@
+require 'faraday'
+
 class FormSubmission < ApplicationRecord
 	self.primary_key = 'objectid'
+	after_save :notify_slack
+	
+	private
+	
+	def notify_slack
+		conn = Faraday.new(url: 'https://hooks.slack.com')
+		response = conn.post do |req|
+		 req.url '/services/T031NFK37/B01K688PFEX/DtF6Dke7QlDnyUgZYBinubrr'
+		 req.headers['Content-Type'] = 'application/json'
+		 req.body = '{ "text": "A new form submission was received!" }'
+		end
+	end
 end
